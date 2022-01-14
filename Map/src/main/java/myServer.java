432a6d7d2fee443590e966c4581187f7b2b77d04
Muntasir-Ayan <94749238+Muntasir-Ayan@users.noreply.github.com>
2022-01-14 23:01:02 +0600

    Add files via upload

---
+++ Map/src/main/java/myServer.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+class MyServer {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Server is waiting for client.");
+            ServerSocket serverSocket = new ServerSocket(6601);
+
+            while (true){
+                Socket sc = serverSocket.accept();
+                Client client = new Client(sc);
+                Thread t = new Thread(client);
+                t.start();
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
+
